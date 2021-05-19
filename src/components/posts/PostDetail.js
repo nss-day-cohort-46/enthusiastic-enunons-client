bf15663f49ba5e4ed11552cf9f58e87234a284d9
emilycentko <emily.centko@gmail.com>
2021-05-19 11:06:32 -0500

    Refactored clinet code to have post page load list of posts

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,33 +1,42 @@
 import React, { useContext, useEffect, useState } from "react"
 import { PostContext } from "./PostProvider"
-import "./Post.css"
 import { useParams, useHistory } from "react-router-dom"
-// import { Link } from "react-router-dom"
+import "./Post.css"
 
 export const PostDetail = (props) => {
-    console.log(props)
-    const { post, getPostById } = useContext(PostContext)
-    const { updatePost, deletePost } = useContext(PostContext)
+    const { posts, getPostById, updatePost, deletePost } = useContext(PostContext)
 
-
-
+    const [post, setPosts] = useState({})
 
     const {postId} = useParams();
     const history = useHistory();
 
     useEffect(() => {
-        console.log("useEffect", postId)
         getPostById(postId)
+        
     }, [])
     
     return (
         
-        <section className="post">
-        <h3 className="post_title">{post.title}</h3>
-        <div className="post_publication_date">Published: {post.publication_date}</div>
+        <section key={`post--${post.id}`} className="post">
+            <h3 className="post__title">{post.title}</h3>
+            <div className="post__publication_date">Published: {post.publication_date}</div>
 
-        <a className="post_image_url" href={ post.image_url } target="_blank">{ post.image_url }</a>
-        <div className="post_content">Content: {post.content}</div>
+            <a className="post_image_url" href={ post.image_url } target="_blank">{ post.image_url }</a>
+            <div className="post_content">Content: {post.content}</div>
+
+            {/* <div className="post_category">{ 
+            categories.map( category => {
+                if (category.id == post.category_id) {
+                    return categories.label
+                }
+            }
+            )} </div> */}
+        
+            <div className="post_imageUrl">{ post.imageUrl }</div>
+
+
+
         <button onClick={() => deletePost(post.id).then(() => props.history.push("/posts"))} >Delete Post</button>
 
             <button onClick={() => {
--- src/components/posts/PostProvider.js
+++ src/components/posts/PostProvider.js
@@ -1,62 +1,68 @@
 import React, { useState, createContext } from "react"
 
-
 export const PostContext = createContext()
 
 export const PostProvider = (props) => {
     const [posts, setPosts] = useState([])
     const [post, setPost] = useState({})
-    const [ searchTerms, setSearchTerms ] = useState("")
-
-  
+    
     const getPosts = () => {
     
-    return fetch("http://localhost:8088/posts")
-        .then(response => response.json())
-        .then(setPosts);
-          
-}
+        return fetch("http://localhost:8000/posts", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setPosts)
+        }
 
-const getPostById = (id) => {
-    return fetch(`http://localhost:8088/posts/${id}`)
-        .then(res => res.json())
-        .then(setPost);
-}
+    const getPostById = (id) => {
+        return fetch(`http://localhost:8000/posts/${id}`, {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+            }})
+                .then(response => response.json())
+    }
 
-const addPost = postObj => {
-    return fetch("http://localhost:8088/posts", {
-        method: "POST",
-        headers: {
+    const addPost = postObj => {
+        return fetch("http://localhost:8000/posts", {
+            method: "POST",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(post)
+        })
+            .then(getPosts)
+    }
+
+    const updatePost = post => {
+        return fetch(`http://localhost:8000/posts/${post.id}`, {
+        method: "PUT",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("rare_user_id")}`,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(postObj)
+        body: JSON.stringify(post)
     })
-    .then(getPosts)
-}
+        .then(getPosts)
+    }
 
-const deletePost = post => {
-    return fetch(`http://localhost:8088/posts/${post}`, {
-      method: "DELETE"
-    }).then(getPosts);
-};
+    const deletePost = postId => {
+        return fetch(`http://localhost:8000/posts/${ postId }/detail`, {
+            method: "DELETE",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
+        })
+            .then(getPosts)
+    }
 
-const updatePost = post => {
-    return fetch(`http://localhost:8088/posts/${post.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(post)
-    }).then(getPosts);
-};
     return (
-// PostContext.Provider allows you to access child components
-
-    <PostContext.Provider value={{
-    // these are the child components of PostContext.Provider
-    posts, post , setPost, getPosts, getPostById, addPost, deletePost, updatePost
-    }}>
-        {props.children}
+        <PostContext.Provider value={{ posts, setPost, getPosts, getPostById,
+            addPost, updatePost, deletePost }}>
+            {props.children}
         </PostContext.Provider>
-)
+    )
 }
